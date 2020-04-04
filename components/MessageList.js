e5a7fdabd39a08928252708e28a1405de5c2edc6
Mark-DSouza <markdsouza434@gmail.com>
2020-04-04 21:11:58 +0530

    Set up dummmy MessageList

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 // import Message from './Message';
+
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+];
 
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-                <div className="help-text">MessageList</div>
+                {DUMMY_DATA.map((element, index) => 
+                    (
+                        <div key={index} className="message">
+                            <div className="message-username">{element.senderId}</div>
+                            <div className="message-text">{element.text}</div>
+                        </div>
+                    )
+                )}
             </div>
-        );
+        )
     }
 }
 
